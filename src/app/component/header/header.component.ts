a661964842d9190d893baf336bef1c56cb09c9da
Associate <Associate@roifmrwindt>
2023-10-11 11:16:37 +0530

    navbar :)

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -3,9 +3,21 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-  
+  navBarDisplay = 'none';
 
+  toggleNavBarDisplay() {
+    this.navBarDisplay = this.navBarDisplay === 'none' ? 'block' : 'none';
+  }
+
+  getNavBarDisplayStyle() {
+    // Use 'block' for smaller screens and 'none' for larger screens
+    return window.innerWidth < 768 ? this.navBarDisplay : 'block';
+  }
+
+  ngOnInit() {
+    this.navBarDisplay = window.innerWidth < 768 ? 'none' : 'block';
+  }
 }
