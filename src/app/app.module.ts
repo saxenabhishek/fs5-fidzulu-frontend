3bb01195ea5eda2604976b535d22eebb7a172a39
Associate <Associate@roifmrwindt>
2023-10-11 09:12:19 +0530

    about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,36 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { HeaderComponent } from './component/header/header.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { AboutUsComponent } from './component/about-us/about-us.component';
+import { TeamMemberCardComponent } from './component/about-us/team-member-card/team-member-card.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import {MatListModule} from '@angular/material/list';
+import {MatGridListModule} from '@angular/material/grid-list';
 
+
+import { MatNativeDateModule, MatRippleModule } from '@angular/material/core';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    AboutUsComponent,
+    TeamMemberCardComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatNativeDateModule,
+    MatRippleModule,
+    MatListModule,
+    MatGridListModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/component/about-us/team-member-card/team-member-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+@Component({
+  selector: 'app-team-member-card',
+  templateUrl: './team-member-card.component.html',
+  styleUrls: ['./team-member-card.component.css']
+})
+export class TeamMemberCardComponent {
+
+}
